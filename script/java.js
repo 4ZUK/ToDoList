9ffe2e686bffab04fc40350c42dc1a1f298737fc
4ZUK <60348334+4ZUK@users.noreply.github.com>
2020-02-06 12:16:38 -0300

    FINISH

--- script/java.js
+++ script/java.js
@@ -1,28 +1,38 @@
-let btnAddTarefa = document.querySelector('#adicionar-tarefa')
-let tarefas = document.querySelector('#tarefas')
-let inputTarefa = document.getElementById('tarefa-digitada')
+let btnAddTarefa = document.querySelector('#adicionar-tarefa');
+let tarefas = document.querySelector('#tarefas');
+let inputTarefa = document.getElementById('tarefa-digitada');
+let botoesAdd = document.querySelectorAll('.botao-tarefa');
 
 
 
+const criarTarefaComEnter = (event) => {
 
+    if (event.keyCode == 13 || event.type == "click") {
+        let valorDigitado = inputTarefa.value
+        if (valorDigitado == "") {
+            alert("Voce deve digitar um valor");
+            return
+        }
 
-
-btnAddTarefa.onclick = function(event){
-        console.log(event.target);
-        
-    let valorDigitado = inputTarefa.value
-
-    let tarefaNova = `<div class="col-md-4">
-    <div class="card-tarefa">
-        <div class="texto-tarefa">
-            ${valorDigitado}
-        </div>
-        <div class="botao-tarefa">
-             <img src="./imagens/chek.png" width="32" alt="tarefa ok" title="tarefa ae">
-        </div>
+        inputTarefa.value = ""
+        let tarefaNova = `<div class="col-md-4">
+<div class="card-tarefa">
+    <div class="texto-tarefa">
+        ${valorDigitado}
     </div>
-    </div> `
+    <div class="botao-tarefa">
+         <img src="./imagens/chek.png" width="32" alt="tarefa ok" title="tarefa ae">
+    </div>
+</div>
+</div> `
+        tarefas.insertAdjacentHTML('beforeend', tarefaNova)
 
+        let objTarefaNova = tarefas.lastElementChild
+        let btnCheckTarefaNova = objTarefaNova.lastElementChild.lastElementChild.onclick = (event) => {
+            event.target.parentNode.parentNode.parentNode.remove()
+        }
 
- tarefas.innerHTML += tarefaNova
+    }
 }
+inputTarefa.addEventListener('keypress', criarTarefaComEnter)
+btnAddTarefa.addEventListener('click', criarTarefaComEnter)
